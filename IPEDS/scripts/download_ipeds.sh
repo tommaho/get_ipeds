81eb9fdc94ac46b3c42e304b7c2c0ddcda1e0fa3
tommaho <tom@tommaho.com>
2023-03-16 13:31:55 -0400

    Initial Commit

---
+++ IPEDS/scripts/download_ipeds.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+#Keeping it simple and old school
+
+path="https://nces.ed.gov/ipeds/datacenter/data/"
+download_path="../downloads"
+data_path="../data"
+dict_path="../dictionaries"
+
+#Downloads files for year(s) specified
+years=("2021" "2020")
+
+#Downloads both the default data file and dictionary for each entry
+#the YYYY character sequence is a placeholder for year loop
+
+files=("HDYYYY" "CYYYY_A")
+
+for ((y=0; y<${#years[@]}; y++)); do
+
+    for ((i=0; i<${#files[@]}; i++)); do
+
+        file=$(echo ${files[$i]/"YYYY"/${years[$y]}}) 
+        #echo ${file}
+        
+        wget -O ${download_path}/${file}.zip ${path}${file}.zip #data
+        wget -O ${download_path}/${file}_Dict.zip ${path}${file}_Dict.zip #dict
+
+        unzip -o ${download_path}/${file}.zip  -d ../data
+        unzip -o ${download_path}/${file}_Dict.zip -d ../dictionaries
+
+    done
+
+done
+
+echo "Finished downloads.."
